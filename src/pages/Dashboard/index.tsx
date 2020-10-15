4fc332238279978f9d0f2deff10cc44852a3965a
Leonardo Figueira <leofigueiracorreia@yahoo.com.br>
2020-10-15 17:45:14 -0300

    connect to github api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,52 +1,61 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { Title, Form, Repositories } from './styles';
 import logoImg from '../../assets/images/logo.svg'
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>):Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt= "Github Explorer" />
       <Title>Explore Repositorios no GitHub</Title>
 
-      <Form>
-        <input placeholder="nome do repositorio" />
+      <Form onSubmit= {handleAddRepository}>
+        <input 
+          placeholder="nome do repositorio"
+          value = {newRepo}
+          onChange = {e => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img src="https://avatars1.githubusercontent.com/u/63420973?s=460&u=024e91a4f5cd27b40a7505a159a29093c7f5fff0&v=4" alt="Profile Picture"/>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="github.com/${repository.full_name}" >
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
 
-          <div>
-            <strong>Leopyy/goBarber</strong>
-            <p>App conceitual do bootcamp</p>
-          </div>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
 
-        <a href="teste">
-          <img src="https://avatars1.githubusercontent.com/u/63420973?s=460&u=024e91a4f5cd27b40a7505a159a29093c7f5fff0&v=4" alt="Profile Picture"/>
-
-          <div>
-            <strong>Leopyy/goBarber</strong>
-            <p>App conceitual do bootcamp</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img src="https://avatars1.githubusercontent.com/u/63420973?s=460&u=024e91a4f5cd27b40a7505a159a29093c7f5fff0&v=4" alt="Profile Picture"/>
-
-          <div>
-            <strong>Leopyy/goBarber</strong>
-            <p>App conceitual do bootcamp</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
       </Repositories>
 
     </>
